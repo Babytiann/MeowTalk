fc3d87f2ad27892b68aafafa2ea40f64213c194f
Meow <meow@gemdzqq.com>
2025-03-15 11:29:37 +0800

    🎉feat: Creating sider navigator

--- frontend/src/components/Sider/Sider.tsx
+++ frontend/src/components/Sider/Sider.tsx
@@ -1,9 +1,12 @@
 import Card from "../Sider/Card.tsx";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import {FormOutlined} from "@ant-design/icons";
+import { useNavigate } from "react-router";
 
 function Sider() {
     const [CardHistory, setCardHistory] = useState<CardHistory[]>([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
         axios.get("http://localhost:5927/CardHistory", { withCredentials: true })
@@ -15,11 +18,27 @@ function Sider() {
 
     }, []);
 
+    function navigatetoHome() {
+        navigate("/home");
+    }
+
     return (
         <div className="min-w-[248px] h-[100vh] bg-[#F9F9F9] rounded-md flex flex-col">
-            {CardHistory.map((item, _) => (
-                <Card key={item.id} sessionId={item.sessionId} msg={item.description} />
-            ))}
+            <div className="h-14 pr-3 flex justify-end items-center">
+                <button className="size-10 px-2 rounded-md flex justify-center items-center
+                                   hover:bg-[#E7E7E7]"
+                      onClick={() => navigatetoHome()}
+                      aria-label="Go to home"
+                >
+                    <FormOutlined className="text-[24px]"/>
+                </button>
+            </div>
+            <div>
+                {CardHistory.map((item, _) => (
+                    <Card key={item.id} sessionId={item.sessionId} msg={item.description} />
+                ))}
+            </div>
+
         </div>
     )
 }
